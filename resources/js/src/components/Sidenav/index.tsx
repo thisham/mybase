b577814df41fc644214a5af00775608353f685bc
Hamdan Yuwafi <hamdanym.wijaya@gmail.com>
2023-03-16 01:40:02 +0800

    chore: add dark mode

--- resources/js/src/components/Sidenav/index.tsx
+++ resources/js/src/components/Sidenav/index.tsx
@@ -46,8 +46,8 @@ function Sidenav(props: SidenavProps): JSX.Element {
       <div className={style.profile_container}>
         <Link to={"/profile"} className={style.profile_picture_container}>
           <img
-            // src="https://i.pinimg.com/564x/51/04/46/5104462af6d4e0781beddbeec4b3ad58.jpg"
-            src="https://i.pinimg.com/564x/81/cb/8a/81cb8aca3f5691f19929a4328831aeb3.jpg"
+            src="https://i.pinimg.com/564x/51/04/46/5104462af6d4e0781beddbeec4b3ad58.jpg"
+            // src="https://i.pinimg.com/564x/81/cb/8a/81cb8aca3f5691f19929a4328831aeb3.jpg"
             alt="Profile Picture"
             className={style.profile_picture}
           />
@@ -66,7 +66,6 @@ function Sidenav(props: SidenavProps): JSX.Element {
           destination={"/projects"}
           icon={GitBranch}
         />
-        {/* <LinkItem code={"DIARY"} title={"Diaries"} destination={"/diaries"} /> */}
         <LinkItem
           code={"GALLERY"}
           title={"Gallery"}
