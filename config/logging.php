8edab82def204fabc25c034358fa9fd1f9606c0d
Hamdan Wijaya <hamdanym.wijaya@gmail.com>
2023-08-11 04:10:39 +0800

    chore: enable logging

--- config/logging.php
+++ config/logging.php
@@ -51,6 +51,11 @@ return [
     */
 
     'channels' => [
+        'dberr' => [
+            'driver' => 'single',
+            'path' => storage_path('logs/dberr.log')
+        ],
+
         'stack' => [
             'driver' => 'stack',
             'channels' => ['single'],
@@ -85,7 +90,7 @@ return [
             'handler_with' => [
                 'host' => env('PAPERTRAIL_URL'),
                 'port' => env('PAPERTRAIL_PORT'),
-                'connectionString' => 'tls://'.env('PAPERTRAIL_URL').':'.env('PAPERTRAIL_PORT'),
+                'connectionString' => 'tls://' . env('PAPERTRAIL_URL') . ':' . env('PAPERTRAIL_PORT'),
             ],
         ],
 
