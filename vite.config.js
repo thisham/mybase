7df90b6f42e2fbca97ac9712fbe8b126c43c8917
Hamdan Wijaya <hamdanym.wijaya@gmail.com>
2023-08-11 03:39:50 +0800

    chore: remove react builder

--- vite.config.js
+++ vite.config.js
@@ -1,28 +1,11 @@
 import { defineConfig } from "vite";
 import laravel from "laravel-vite-plugin";
-import reactRefresh from "@vitejs/plugin-react-refresh";
 
-export default defineConfig(({ command }) => ({
-    base: command == "serve" ? "" : "build",
-    publicDir: "",
-    build: {
-        manifest: true,
-        outDir: "public/build",
-        rollupOptions: {
-            input: "resources/js/app.js",
-        },
-    },
+export default defineConfig(() => ({
     plugins: [
         laravel({
             input: ["resources/css/app.css", "resources/js/app.js"],
             refresh: true,
         }),
-        reactRefresh(),
     ],
-    resolve: {
-        alias: {
-            "@resources": "/resources",
-            "@src": "/resources/js/src",
-        },
-    },
 }));
