82cc5e22340c3a4ae4fd17059a801cc7474f2e7c
Hamdan Wijaya <hamdanym.wijaya@gmail.com>
2023-08-11 12:03:11 +0800

    chore: add user-layout component

---
+++ resources/views/components/layouts/user.blade.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>{{ $attributes['title'] }}</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=K2D&display=swap" rel="stylesheet">
+    @vite('resources/css/app.css')
+    @vite('resources/js/app.js')
+</head>
+
+<body class="min-h-screen flex flex-col">
+    <div class="topnav">
+        <div class="button-container">
+            <a class="clickable @if (Route::is('main.dashboard')) active @endif" href="{{ route('main.dashboard') }}">
+                {{ __('display.navigation.dashboard') }}
+            </a>
+            <a class="clickable @if (Route::is('auth.sign-in')) active @endif" href="{{ route('auth.sign-in') }}">
+                {{ __('display.navigation.financial') }}
+            </a>
+            <a class="clickable @if (Route::is('auth.sign-out')) active @endif" href="{{ route('auth.sign-out') }}">
+                {{ __('display.navigation.sign-out') }}
+            </a>
+        </div>
+    </div>
+
+    <div class="px-12 mt-20 mb-auto">
+        {{ $slot }}
+    </div>
+
+    <footer class="w-full h-12 flex items-center justify-center">
+        &copy; 2023 Divisi Anggaran Keuangan
+    </footer>
+</body>
+
+</html>
