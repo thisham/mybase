650c9247bd23087161fabf9303902d8af1199bf2
Hamdan Wijaya <hamdanym.wijaya@gmail.com>
2023-08-13 14:11:48 +0800

    fix(frontend): incorrect data displayed

--- resources/views/pages/loan.blade.php
+++ resources/views/pages/loan.blade.php
@@ -5,6 +5,9 @@
         <div class="flex flex-col gap-2 mb-4">
             <div class="flex justify-between items-center">
                 <h1>{{ __('display.financial.loans') }}</h1>
+                <a href="{{ route('financial.create-loan') }}" class="clickable primary h-fit w-fit text-sm">
+                    {{ __('display.action.add') }}
+                </a>
             </div>
 
             @if (Session::has('success'))
@@ -21,9 +24,9 @@
                 <thead>
                     <tr class="border-b-2 border-item-400">
                         <th class="py-4 text-left pl-2 w-16">No.</th>
-                        <th class="py-4 text-left pl-2">{{ __('display.field-column.source') }}</th>
+                        <th class="py-4 text-left pl-2">{{ __('display.field-column.account') }}</th>
                         <th class="py-4 text-left pl-2">{{ __('display.field-column.value') }}</th>
-                        <th class="py-4 text-left pl-2">{{ __('display.field-column.reduction') }}</th>
+                        <th class="py-4 text-left pl-2">{{ __('display.field-column.billing') }}</th>
                         <th class="py-4 text-left pl-2">{{ __('display.field-column.status') }}</th>
                         <th class="py-4 text-left pl-2"></th>
                     </tr>
@@ -33,17 +36,17 @@
                     @forelse ($loansList as $index => $record)
                         <tr class="even:bg-item-200">
                             <td class="pl-4">{{ $index + 1 }}</td>
-                            <td>{{ $record->source }}</td>
+                            <td>{{ $record->account }}</td>
                             <td>{{ $record->value }}</td>
-                            <td>{{ $record->reduction }}</td>
+                            <td>{{ $record->billing }}</td>
                             <td>{{ $record->finalized_at ? __('display.status.finalized') : __('display.status.floating') }}
                             </td>
                             <td>
                                 <div class="flex">
-                                    <a href="{{ route('financial.update-loans', ['id' => $record->id]) }}"
+                                    {{-- <a href="{{ route('financial.update-loan', ['id' => $record->id]) }}"
                                         class="clickable ghost w-fit">{{ __('display.action.edit') }}</a>
-                                    <a href="{{ route('financial.delete-loans', ['id' => $record->id]) }}"
-                                        class="clickable ghost w-fit">{{ __('display.action.remove') }}</a>
+                                    <a href="{{ route('financial.delete-loan', ['id' => $record->id]) }}"
+                                        class="clickable ghost w-fit">{{ __('display.action.remove') }}</a> --}}
                                 </div>
                             </td>
                         </tr>
