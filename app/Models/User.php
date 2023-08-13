2829657e255d683aa12e9f24e0ed1a653b789ce2
Hamdan Wijaya <hamdanym.wijaya@gmail.com>
2023-08-13 08:47:03 +0800

    chore: add user session data cast for ID

--- app/Models/User.php
+++ app/Models/User.php
@@ -39,6 +39,7 @@ class User extends Authenticatable
      * @var array<string, string>
      */
     protected $casts = [
+        'id' => 'string',
         'email_verified_at' => 'datetime',
     ];
 }
